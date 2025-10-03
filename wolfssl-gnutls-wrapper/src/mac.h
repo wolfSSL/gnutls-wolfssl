e9c2e4c948dc470505c7b39b65c6272401217325
Reda Chouk <reda@wolfssl.com>
2025-10-03 12:09:04 +0200

    - Skeleton code to check for AES-CTR support, if available, AES-SIV is   also available. - Completed encryption and decryption implementation with s2v routine to perform the   AES-CMAC operations. - Currently working on testing & fixing bugs that I find along the way;

--- wolfssl-gnutls-wrapper/src/mac.h
+++ wolfssl-gnutls-wrapper/src/mac.h
@@ -1,4 +1,22 @@
 #ifdef ENABLE_WOLFSSL
+#include <wolfssl/wolfcrypt/cmac.h>
+#include <wolfssl/wolfcrypt/aes.h>
+
 int wolfssl_mac_register(void);
 int get_hash_type(gnutls_mac_algorithm_t algorithm);
+/** Context for wolfSSL CMAC. */
+struct wolfssl_cmac_ctx {
+    /** wolfSSL CMAC object. */
+    Cmac cmac_ctx;
+    /** Indicates that this context as been initialized. */
+    int initialized;
+    /** The GnuTLS cipher algorithm ID. */
+    gnutls_mac_algorithm_t algorithm;
+    /** Cached key. */
+    unsigned char key[AES_256_KEY_SIZE];
+    /** Size of cached key. */
+    size_t key_size;
+    /** Setting of the key is required before hashing. */
+    unsigned int set_key:1;
+};
 #endif
